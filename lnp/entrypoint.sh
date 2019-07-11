4b2fec54a1ccd959458f6d3d1b98d57b89964e58
liangpingzheng <liangpingzheng@5i5j.com>
2019-07-11 14:47:10 +0800

    php7.1-fpm nginx安装配置

--- lnp/entrypoint.sh
+++ lnp/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Don't continue if we catch an error.
 set -e
-/etc/init.d/nginx start
-/etc/init.d/php7.2-fpm start
+ /etc/init.d/nginx start
+ /etc/init.d/php7.1-fpm start
 # just keep this script running
 while [[ true ]]; do
     sleep 1
